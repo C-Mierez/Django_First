c57774d9bab76cc6893b12c2fe53e46255f56a6a
CMierez <carlos.mierez20@gmail.com>
2021-01-06 15:29:37 -0300

    Deleted src folder

--- src/Profiles_API/urls.py
+++
@@ -1,7 +0,0 @@
-
-from django.urls import path
-from Profiles_API import views
-
-urlpatterns = [
-    path('hello-view/', views.HelloApiView.as_view()),
-]
--- src/notes/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Note(models.Model):
-    title           = models.CharField(max_length=60, null=False)
-    content         = models.TextField()
-    is_Completed    = models.BooleanField(default=False)
-    
-    def __str__(self):
-        return self.title
